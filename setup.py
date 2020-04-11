fb5667e5c85e2eaf6f9a42af8758d13ecb5b1d7f
Y4hL <43219534+Y4hL@users.noreply.github.com>
2020-04-11 14:19:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 import sys
 from cx_Freeze import setup, Executable
 
-include_files = []
 base = None
 
 if sys.platform == "win32":
     base = "Win32GUI"
 
+build_exe_options = {
+    # Packages to include
+    'includes': [],
+    # Files to include
+    'include_files': []
+}
+
 setup(name="puzzle",
       version="0.1",
       description="Fun computer game",
-      options={'build_exe': {'include_files': include_files}},
+      options={'build_exe': build_exe_options},
       executables=[Executable("client.py", base=base)])
 
 # python setup.py build
