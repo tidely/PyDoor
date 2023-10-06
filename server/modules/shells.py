f4a4f7e48b2f223a2b0353235e74ede6f3f60c19
Y4hL <43219534+Y4hL@users.noreply.github.com>
2023-10-06 17:35:03 +0300

    Remove hardcoded timeout limit

--- server/modules/shells.py
+++ server/modules/shells.py
@@ -5,28 +5,28 @@ from utils.timeout_handler import timeoutsetter
 from modules.clients import Client
 
 
-def shell(client: Client, command: str) -> str:
+def shell(client: Client, command: str, timeout: float | None = 60.0) -> str:
     """ Run shell command on client """
     logging.debug("Sending shell command '%s' to client (%s)", command, client.id)
 
     client.write(b"SHELL")
     client.write(command.encode())
 
-    with timeoutsetter(client, 60):
+    with timeoutsetter(client, timeout):
         response = client.read().decode()
 
     logging.debug("Command (%s) output from client (%s): %s", command, client.id, response)
     return response
 
 
-def python(client: Client, command: str) -> str:
+def python(client: Client, command: str, timeout: float | None = 60.0) -> str:
     """ Run python command on client """
     logging.debug("Sending python command '%s' to client (%s)", command, client.id)
 
     client.write(b"PYTHON")
     client.write(command.encode())
 
-    with timeoutsetter(client, 60):
+    with timeoutsetter(client, timeout):
         response = client.read().decode()
 
     logging.debug("Python command '%s' output from client (%s): %s", command, client.id, response)
